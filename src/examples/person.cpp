12d008a1d38df5ab4008b4851e019336e4da661d
Billie Bayless <bbayless@ad.ssucet>
2025-01-15 13:19:36 -0500

    initial commit of vs code

---
+++ src/examples/person.cpp
@@ -0,0 +1,10 @@
+#include "person.h"    //why "" instead of <>??
+
+//We're providing the BODY / DEFINITION of the method we declared in our h file
+float Person::calculate_wage()
+{
+	//In any (non-static) method, we have access to all atrributes for this
+	//instance. Our job in this method is to tell the compiler HOW to 
+	//calculate a wage using our attributes
+	return hourly_rate * hours_worked;
+}
